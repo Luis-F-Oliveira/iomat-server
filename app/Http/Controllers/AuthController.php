4caf09171f0e268ab231a36a5b0b2c938dab0e96
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-07-09 15:00:32 -0300

    refatorando AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\RolesOnUsers;
@@ -11,28 +12,43 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $entry_code = $request->input('entry_code');
-        $user = User::where('entry_code', $entry_code)->first();
-        $roles_on_users = RolesOnUsers::with('role')
-                            ->where('user_id', $user->id)
-                            ->get();
+        try {
+            $validated = $request->validate([
+                'entry_code' => 'required|string|min:6|max:6',
+            ]);
 
-        $abilities = $roles_on_users->pluck('role.name')->toArray();
-        $token = $user->createToken('auth-token', $abilities)->plainTextToken;
-        $cookie = cookie('jwt', $token, 60 * 24);
+            $entry_code = $validated['entry_code'];
+            $user = User::where('entry_code', $entry_code)->first();
+            $roles_on_users = RolesOnUsers::with('role')
+                ->where('user_id', $user->id)
+                ->get();
 
-        return response()->json([
-            'user' => $user,
-            'token' => $token
-        ])->withCookie($cookie);
+            $abilities = $roles_on_users->pluck('role.name')->toArray();
+            $token = $user->createToken('auth-token', $abilities)->plainTextToken;
+            $cookie = cookie('jwt', $token, 60 * 24);
+
+            return response()->json([
+                'token' => $token
+            ])->withCookie($cookie);
+        } catch(Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function logout(Request $request)
     {
-        $cookie = Cookie::forget('jwt');
+        try {
+            $cookie = Cookie::forget('jwt');
 
-        return response()->json([
-            'message' => 'success'
-        ], 200)->withCookie($cookie);
+            return response()->json([
+                'message' => 'User logged out'
+            ], 200)->withCookie($cookie);
+        } catch(Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
